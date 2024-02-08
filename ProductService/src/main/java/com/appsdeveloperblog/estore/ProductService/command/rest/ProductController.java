d719352c899886d2cf3c2e82788ea1d000fe2217
Yana Yasinvski <jantschick3@gmail.com>
2024-02-08 14:06:00 +0100

    first commit

---
+++ ProductService/src/main/java/com/appsdeveloperblog/estore/ProductService/command/rest/ProductController.java
@@ -0,0 +1,59 @@
+package com.appsdeveloperblog.estore.ProductService.command.rest;
+
+import com.appsdeveloperblog.estore.ProductService.command.CreateProductCommand;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+
+    private final Environment env;
+    private final CommandGateway commandGateway;
+
+    @PostMapping
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString())
+                .build();
+
+        String returnValue;
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception ex) {
+            returnValue = ex.getLocalizedMessage();
+        }
+
+        return returnValue;
+    }
+
+    @GetMapping
+    public String getProduct() {
+        return "HTTP GET Handled " + env.getProperty("local.server.port");
+    }
+
+    @PutMapping
+    public String updateProduct() {
+        return "HTTP PUT Handled";
+    }
+
+    @DeleteMapping
+    public String deleteProduct() {
+        return "HTTP DELETE Handled";
+    }
+}
